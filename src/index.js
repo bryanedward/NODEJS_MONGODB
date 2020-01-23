7bd22b91f8071407cf41c9df5efef38d86b46be8
bryanedward <46612406+bryanedward@users.noreply.github.com>
2020-01-23 16:38:27 -0500

    actualizacion del config

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@
 var mongoose = require('mongoose');
 //la configuracion donde fue hecha
 var app = require('./app');
-var port = 3700;
-var MONGODB_URI = 'mongodb://heroku_d552rbjp:nrq7heatih0gh5qc5s6k5cbs2s@ds213079.mlab.com:13079/heroku_d552rbjp'; 
+const config = require('./config');
+
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URI, {useNewUrlParser : true})
+
+mongoose.connect(config.db, {useNewUrlParser : true})
     .then(()=>{
         //crear el servidor
-        app.listen(port,()=>{
+        app.listen(config.port,()=>{
             console.log("servidor corriendo perfectamente");
         });
     })
