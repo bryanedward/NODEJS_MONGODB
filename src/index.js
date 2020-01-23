67c8eddfbf97068ae02d3b09f0aee6afffe132c8
bryanedward <46612406+bryanedward@users.noreply.github.com>
2020-01-23 14:36:41 -0500

    ok

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ var mongoose = require('mongoose');
 //la configuracion donde fue hecha
 var app = require('./app');
 var port = 3700;
-var MONGODB_URI = 'mongodb://heroku_d552rbjp:nrq7heatih0gh5qc5s6k5cbs2s@ds213079.mlab.com:13079/heroku_d552rbjp';
-
-
+var MONGODB_URI = 'mongodb://heroku_d552rbjp:nrq7heatih0gh5qc5s6k5cbs2s@ds213079.mlab.com:13079/heroku_d552rbjp'; 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGODB_URI, {useNewUrlParser : true})
     .then(()=>{
